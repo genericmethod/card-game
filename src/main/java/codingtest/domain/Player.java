86c6795eea5dbf35d76451d21df3f395be7779db
cfarrugia <chris@yobetit.com>
2016-01-13 22:05:30 +0100

    calculating next player move.

--- src/main/java/codingtest/domain/Player.java
+++ src/main/java/codingtest/domain/Player.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class Player {
   Move nextMove;
-  List<Card> cards = new ArrayList<Card>();
 
+  List<Card> cards = new ArrayList<Card>();
   public void addCard(Card card){
     cards.add(card);
   }
@@ -17,6 +17,18 @@ public class Player {
     for (Card card : cards) {
       total = total + card.getRank().value;
     }
+    nextMove = calculateNextMove(total);
     return total;
+  }
+
+  private Move calculateNextMove(int total) {
+    if (total < 17) return Move.HIT;
+    if (total > 21) return Move.BUST;
+    if (total >=17) return Move.STICK;
+    return null;
+  }
+
+  public Move getNextMove() {
+    return nextMove;
   }
 }
--- src/test/java/codingtest/domain/PlayerTest.java
+++ src/test/java/codingtest/domain/PlayerTest.java
@@ -14,12 +14,19 @@ public class PlayerTest {
     Player player = new Player();
 
     assertEquals(0, player.getCardTotal());
+    assertEquals(Move.HIT, player.getNextMove());
 
     player.addCard(new Card(Rank.ACE, Suit.CLUBS));
     assertEquals(11, player.getCardTotal());
+    assertEquals(Move.HIT, player.getNextMove());
 
     player.addCard(new Card(Rank.EIGHT, Suit.CLUBS));
     assertEquals(19, player.getCardTotal());
+    assertEquals(Move.STICK, player.getNextMove());
+
+    player.addCard(new Card(Rank.THREE, Suit.CLUBS));
+    assertEquals(22, player.getCardTotal());
+    assertEquals(Move.BUST, player.getNextMove());
 
   }
 }
