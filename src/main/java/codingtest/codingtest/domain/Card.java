8fceea90d372218f1ba95be7e4b5bdae08931629
cfarrugia <chris@yobetit.com>
2016-01-13 20:43:39 +0100

    Initial commit for Suit, Rank and Card

--- src/main/java/codingtest/codingtest/domain/Card.java
+++ src/main/java/codingtest/codingtest/domain/Card.java
@@ -1,7 +1,26 @@
 package codingtest.domain;
 
+import codingtest.codingtest.domain.Rank;
+import codingtest.codingtest.domain.Suit;
+
 /**
  * This is the domain class that represents a card in a card game.
  */
 public class Card {
+
+  private Rank rank;
+  private Suit suit;
+
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  public Rank getRank() {
+    return rank;
+  }
+
+  public Suit getSuit() {
+    return suit;
+  }
 }
---
+++ src/main/java/codingtest/codingtest/domain/Suit.java
@@ -0,0 +1,9 @@
+package codingtest.codingtest.domain;
+
+
+public enum Suit{
+  HEARTS,
+  DIAMONDS,
+  CLUBS,
+  SPADES
+}
